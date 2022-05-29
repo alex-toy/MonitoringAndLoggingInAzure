dd791fdd50c22116db69b7b75d3328bbe42da51c
alex-toy <alexei.80@hotmail.fr>
2022-05-29 19:48:50 +0200

    cleaning

---
+++ commands/deploy_app.ps1
@@ -0,0 +1,18 @@
+# Variables
+$Global:Subscription = "alexeisubscription"
+$Global:Subscription = "Azure subscription 1"
+
+#resource Group :
+#southcentralus centralus francecentral westus2 eastus
+$Global:RGLocation = "francecentral"
+$Global:RGName = "webapp-rg"
+
+#General Settings :"
+$Global:AppPath = ".\app"
+
+#######################################################################
+# Steps :
+
+az group create --name $RGName --location $RGLocation
+
+."commands\WebApp\WebApp_create.ps1"
